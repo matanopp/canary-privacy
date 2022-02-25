4d6ab589f217f751b1182ec5079bbb6fc263485e
Matan Oppenheim <matanopp@gmail.com>
2022-02-24 20:06:31 -0500

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "canaryprivacy21cc3244": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
