d84d407f55bd2ee23e1b048095eab2328e5c8a1c
Matan Oppenheim <matanopp@gmail.com>
2022-05-06 14:04:04 -0400

    added all cookies feature

--- src/Overview.js
+++ src/Overview.js
@@ -3,14 +3,15 @@ import OverviewTile from './OverviewTile.js';
 import pageImage from './images/page.svg';
 import scriptImage from './images/script.svg';
 import formImage from './images/form.svg';
+import cookieImage from './images/cookie.svg';
 
 function Overview(props) {
     return (
         <div className="overview-container">
-            <OverviewTile data={props.pages} total={props.pagesTotal} img={pageImage} message={"New pages last week"} linkTo="/pages" />
-            <OverviewTile data={props.scripts} total={props.scriptsTotal} img={scriptImage} message={"New scripts last week"} linkTo="/scripts" />
-            <OverviewTile data={props.forms} total={props.formsTotal} img={formImage} message={"New forms last week"} linkTo="/forms" />
-
+            <OverviewTile data={props.pages} total={props.pagesTotal} img={pageImage} message={"New pages"} linkTo="/pages" />
+            <OverviewTile data={props.scripts} total={props.scriptsTotal} img={scriptImage} message={"New scripts"} linkTo="/scripts" />
+            <OverviewTile data={props.forms} total={props.formsTotal} img={formImage} message={"New forms"} linkTo="/forms" />
+            <OverviewTile data={props.cookies} total={props.cookiesTotal} img={cookieImage} message={"New cookies"} linkTo="/allCookies" />
         </div>
     );
 }
